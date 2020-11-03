acfa74de27b772c2dd8f20d5f8d90b2591043245
Boyka <upcvagen@163.com>
2020-11-03 21:49:27 +0800

    url manager

--- utils/url_manager.py
+++ utils/url_manager.py
@@ -4,7 +4,7 @@ class UrlManager():
         self.old_urls = set()
 
     def add_new_url(self, url):
-        if url is None or len(url)==0:
+        if url is None or len(url) == 0:
             return
         elif url in self.new_urls or url in self.old_urls:
             return
@@ -12,10 +12,32 @@ class UrlManager():
             self.new_urls.add(url)
 
     def add_new_urls(self, urls):
-        pass
+        if urls is None or len(urls) == 0:
+            return
+        else:
+            for url in urls:
+                self.add_new_url(url)
 
     def get_url(self):
-        pass
+        if self.has_new_url():
+            url = self.new_urls.pop()
+            self.old_urls.add(url)
+            return url
+        else:
+            return None
 
     def has_new_url(self):
-        pass
+        return len(self.new_urls) > 0
+if __name__ == '__main__':
+    url_manager=UrlManager()
+    url_manager.add_new_url("url1")
+    url_manager.add_new_urls(["url1","url2"])
+    print(url_manager.new_urls,url_manager.old_urls)
+    print("#"*30)
+    new_url=url_manager.get_url()
+    print(url_manager.new_urls, url_manager.old_urls)
+    print("#" * 30)
+    new_url = url_manager.get_url()
+    print(url_manager.new_urls, url_manager.old_urls)
+    print("#" * 30)
+    print(url_manager.has_new_url())
