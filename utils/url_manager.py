5e025f6ce6d11dd531ea7307d1cda96e4a58b589
Boyka <upcvagen@163.com>
2020-11-04 09:38:07 +0800

    url manager

--- utils/url_manager.py
+++ utils/url_manager.py
@@ -28,13 +28,15 @@ class UrlManager():
 
     def has_new_url(self):
         return len(self.new_urls) > 0
+
+
 if __name__ == '__main__':
-    url_manager=UrlManager()
+    url_manager = UrlManager()
     url_manager.add_new_url("url1")
-    url_manager.add_new_urls(["url1","url2"])
-    print(url_manager.new_urls,url_manager.old_urls)
-    print("#"*30)
-    new_url=url_manager.get_url()
+    url_manager.add_new_urls(["url1", "url2"])
+    print(url_manager.new_urls, url_manager.old_urls)
+    print("#" * 30)
+    new_url = url_manager.get_url()
     print(url_manager.new_urls, url_manager.old_urls)
     print("#" * 30)
     new_url = url_manager.get_url()
